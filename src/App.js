f2f1eb4d744e2b9f843609cc3c75f7a38149405e
rxavio <rucahoxavi@gmail.com>
2022-03-14 11:50:58 +0200

    hook react video

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-import { ReactMediaRecorder } from "react-media-recorder";
+// import { ReactMediaRecorder } from "react-media-recorder";
 
-const App = () => (
-  <div>
-    <ReactMediaRecorder
-      screen
-      render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-        <div>
-          <p>{status}</p>
-          <button onClick={startRecording}>Start Recording</button>
-          <button onClick={stopRecording}>Stop Recording</button>
-          <video src={mediaBlobUrl} controls autoPlay loop />
-        </div>
-      )}
-    />
-  </div>
-);
+// const App = () => (
+//   <div>
+//     <ReactMediaRecorder
+//       video
+//       render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
+//         <div>
+//           <p>{status}</p>
+//           <button onClick={startRecording}>Start Recording</button>
+//           <button onClick={stopRecording}>Stop Recording</button>
+//           <video src={mediaBlobUrl} controls autoPlay loop />
+//         </div>
+//       )}
+//     />
+//   </div>
+// );
 
-export default App
+// export default App
+
+
+// import { ReactMediaRecorder, useReactMediaRecorder } from "react-media-recorder";
+
+// function App(){
+//   const{ status, startRecording, stopRecording, mediaBlobUrl }=useReactMediaRecorder({video:true})
+
+//      return(
+//       <div>
+//       <p>{status}</p>
+//       <button onClick={startRecording}>Start Recording</button>
+//       <button onClick={stopRecording}>Stop Recording</button>
+//       <video src={mediaBlobUrl} controls autoPlay loop />
+//       </div>
+//      )}
+// export default App
+
+
+
+import { useReactMediaRecorder } from "react-media-recorder";
+
+const RecordView = () => {
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ video: true });
+
+  return (
+    <div>
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+    </div>
+  );
+};
+
+export default RecordView
+
+
