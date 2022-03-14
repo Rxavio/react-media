ee67478767a33e67b032a17cd105b3dd2f8902b2
rxavio <rucahoxavi@gmail.com>
2022-03-14 17:12:00 +0200

    take photo

--- src/App.js
+++ src/App.js
@@ -34,26 +34,58 @@
 //      )}
 // export default App
 
-import { useReactMediaRecorder } from "react-media-recorder";
+// import { useReactMediaRecorder } from "react-media-recorder";
 
-const RecordView = () => {
-  const {
-    status,
-    startRecording,
-    stopRecording,
-    mediaBlobUrl,
-  } = useReactMediaRecorder({ screen: true });
+// const RecordView = () => {
+//   const {
+//     status,
+//     startRecording,
+//     stopRecording,
+//     mediaBlobUrl,
+//   } = useReactMediaRecorder({ screen: true });
 
-  return (
-    <div>
-      <p>{status}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
-      <video src={mediaBlobUrl} controls autoPlay loop />
-    </div>
-  );
+//   return (
+//     <div>
+//       <p>{status}</p>
+//       <button onClick={startRecording}>Start Recording</button>
+//       <button onClick={stopRecording}>Stop Recording</button>
+//       <video src={mediaBlobUrl} controls autoPlay loop />
+//     </div>
+//   );
+// };
+
+// export default RecordView
+
+
+import React from "react";
+import Webcam from "react-webcam";
+
+const WebcamComponent = () => <Webcam />;
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
 };
 
-export default RecordView
+const WebcamCapture = () => (
+  <Webcam
+    audio={false}
+    height={720}
+    screenshotFormat="image/jpeg"
+    width={1280}
+    videoConstraints={videoConstraints}
+  >
+    {({ getScreenshot }) => (
+      <button
+        onClick={() => {
+          const imageSrc = getScreenshot()
+        }}
+      >
+        Capture photo
+      </button>
+    )}
+  </Webcam>
+);
 
+export default WebcamCapture
 
